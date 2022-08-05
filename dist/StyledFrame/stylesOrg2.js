7f98ca77d7ae9a940d0890c605eb0b66c785d2ca
Vladimir Stepin <vladimir.stepin@osinit.com>
2022-08-05 11:38:37 +0300

    EC-130 styles repair no media query

--- dist/StyledFrame/stylesOrg2.js
+++ dist/StyledFrame/stylesOrg2.js
@@ -42,8 +42,8 @@ export default function styles(theme) {
         marginLeft: 'auto',
         marginRight: 'auto',
         maxWidth: '794px',
-        paddingLeft: '15px',
-        paddingRight: '15px',
+        paddingLeft: '60px',
+        paddingRight: '60px',
         paddingBottom: '12px',
         backgroundColor: '#BFD7B5'
       },
@@ -52,25 +52,32 @@ export default function styles(theme) {
         margin: '0 auto'
       },
       '& .o-cover__2': {
-        fontSize: '40px',
+        fontSize: '53px',
+        marginTop: '46px',
         color: '#1A322C'
       },
       '& .o-cover__3': {
-        fontSize: '22px',
+        fontSize: '28px',
+        marginTop: '-16px',
         lineHeight: 1.3
       },
       '& .o-cover__4': {
         fontSize: '16px',
-        color: '#007455'
+        color: '#007455',
+        marginTop: '34px'
       },
       '& .o-cover__5': {
-        fontSize: '15px'
+        fontSize: '15px',
+        marginTop: '34px'
       },
       '& .o-cover__6, & .o-cover__8': {
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         fontFamily: 'Arial, sans-serif',
         fontSize: '15px'
+      },
+      '& .o-cover__7': {
+        marginTop: '12px'
       },
       '& .o-cover__8': {
         backgroundColor: '#fff',
@@ -84,7 +91,12 @@ export default function styles(theme) {
         marginTop: '5px'
       },
       '& .o-cover__13': {
-        marginTop: '5px'
+        marginTop: '0'
+      },
+      '& .o-cover__13:nth-of-type(1n+2)': {
+        marginLeft: '5px',
+        marginTop: '0',
+        verticalAlign: 'bottom'
       },
       '& .o-cover__15': {
         backgroundColor: '#fff',
@@ -92,33 +104,47 @@ export default function styles(theme) {
         marginTop: '-12px'
       },
       '& .o-cover__16': {
-        marginTop: '20px'
+        marginTop: '38px'
+      },
+      '& .o-cover__16:nth-of-type(1n+2)': {
+        marginLeft: '65px'
       },
       '& .o-cover__17': {
-        width: '100px'
+        width: '184px'
       },
       '& .o-cover__18': {
         marginTop: '0',
-        marginLeft: '12px'
+        marginLeft: '0',
+        textAlign: 'center',
+        fontSize: '27px'
       },
       '& .o-details': {
         boxSizing: 'border-box',
         marginLeft: 'auto',
         marginRight: 'auto',
         maxWidth: '794px',
-        paddingLeft: '15px',
-        paddingRight: '15px',
+        paddingLeft: '30px',
+        paddingRight: '30px',
         paddingTop: '15px',
         paddingBottom: '15px'
       },
       '& .o-details__1': {
-        display: 'flex'
+        display: 'flex',
+        paddingTop: '20px'
       },
       '& .o-details__2 img': {
-        display: 'none'
+        flex: '0 1 auto',
+        width: '114px',
+        display: 'block'
       },
       '& .o-details__4:nth-of-type(1n+2)': {
-        flex: '1 1 0%'
+        flex: '1 1 0%',
+        marginLeft: '32px'
+      },
+      '& .o-details__5': {
+        marginLeft: '146px',
+        marginTop: '12px',
+        marginBottom: '6px'
       },
       '& .o-details__6': {
         padding: '5px',
@@ -127,7 +153,7 @@ export default function styles(theme) {
       },
       '& .o-details__7': {
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'row'
       },
       '& .o-details__8, & .o-details__9': {
         flex: '1 1 0%',
@@ -136,26 +162,35 @@ export default function styles(theme) {
         fontWeight: '700'
       },
       '& .o-details__9:nth-of-type(1n+2)': {
-        marginTop: '20px'
+        marginLeft: '153px',
+        marginTop: '0'
       },
       '& .o-details__10': {
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'row'
       },
       '& .o-details__11': {
-        marginBottom: '20px'
+        marginBottom: '0',
+        flex: '0 1 auto',
+        textAlign: 'right'
       },
       '& .o-details__12': {
-        fontFamily: 'Arial, sans-serif'
+        fontFamily: 'Arial, sans-serif',
+        flex: '1 1 0%'
+      },
+      '& .o-details__12:nth-of-type(1n+2)': {
+        marginLeft: '20px'
       },
       '& .o-details__13': {
         color: '#007455',
-        fontSize: '22px'
+        fontSize: '22px',
+        fontFamily: 'Arial, sans-serif',
+        marginTop: '-4px'
       },
       '& .o-details__14': {
-        marginTop: '20px',
+        marginTop: '58px',
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         fontFamily: 'Arial, sans-serif',
         fontSize: '12px'
       },
@@ -165,7 +200,10 @@ export default function styles(theme) {
         fontWeight: '700'
       },
       '& .o-details__15': {
-        flex: '1 1 0%'
+        flex: '0 0 400px'
+      },
+      '& .o-details__18': {
+        padding: '0 7px 0 24px'
       },
       '& .o-details__16': {
         flex: '1 1 0%'
@@ -175,7 +213,10 @@ export default function styles(theme) {
         margin: '0 auto'
       },
       '& .o-details__16:nth-of-type(1n+2)': {
-        marginTop: '20px'
+        marginTop: '0'
+      },
+      '& .o-details__17': {
+        paddingLeft: '42px'
       },
       '& .o-details__17 p': {
         margin: '7px 0 7px'
@@ -193,7 +234,10 @@ export default function styles(theme) {
         marginBottom: '0'
       },
       '& .o-details__20, & .o-details__19': {
-        marginBottom: '7px'
+        marginBottom: '10px'
+      },
+      '& p.o-details__19': {
+        paddingLeft: '42px'
       },
       '& .o-details__21': {
         overflow: 'auto',
@@ -212,6 +256,9 @@ export default function styles(theme) {
         backgroundColor: '#ddd',
         textAlign: 'left'
       },
+      '& .o-details__21--2': {
+        marginTop: '70px'
+      },
       '& .o-details__21--2 tr:first-child': {
         backgroundColor: '#ddd',
         verticalAlign: 'top'
@@ -222,9 +269,17 @@ export default function styles(theme) {
       '& .o-details__21--3 table th': {
         border: 'none'
       },
+      '& .o-details__22': {
+        marginTop: '90px'
+      },
+      '& .o-details__23': {
+        marginBottom: '16px'
+      },
       '& .o-details__24': {
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative'
       },
       '& .o-details__25': {
         flex: '1 1 0%',
@@ -234,7 +289,8 @@ export default function styles(theme) {
       '& .o-details__26': {
         boxSizing: 'border-box',
         flex: '0 1 auto',
-        marginTop: '20px'
+        marginTop: '0',
+        marginLeft: '5px'
       },
       '& .o-details__27': {
         fontSize: '20px',
@@ -251,9 +307,19 @@ export default function styles(theme) {
         marginRight: 'auto',
         maxWidth: '794px'
       },
+      '& .o-info__1': {
+        marginTop: '14px'
+      },
+      '& .o-info__2': {
+        marginTop: '35px'
+      },
       '& .o-info__3': {
         fontSize: '19px',
-        color: '#007455'
+        color: '#007455',
+        marginTop: '30px'
+      },
+      '& .o-info__4': {
+        marginTop: '8px'
       },
       '& .o-info__6 img': {
         display: 'block',
@@ -261,8 +327,8 @@ export default function styles(theme) {
       },
       '& .o-info__top, & .o-info__bottom': {
         boxSizing: 'border-box',
-        paddingLeft: '15px',
-        paddingRight: '15px',
+        paddingLeft: '30px',
+        paddingRight: '30px',
         paddingTop: '20px',
         paddingBottom: '15px'
       },
@@ -271,11 +337,16 @@ export default function styles(theme) {
       },
       '& .o-info__5': {
         marginBottom: '0',
+        marginTop: '30px',
         fontSize: '19px',
         color: '#007455'
       },
+      '& .o-info__7': {
+        marginTop: '30px'
+      },
       '& .o-info__10:nth-of-type(1n+2)': {
-        marginTop: '20px'
+        marginTop: '0',
+        marginLeft: '10px'
       },
       '& .o-info__10 a': {
         display: 'flex',
@@ -293,16 +364,23 @@ export default function styles(theme) {
       },
       '& .o-info__11': {
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'row',
+        marginTop: '24px'
       },
       '& .o-info__9': {
         fontSize: '14px',
-        flex: '1 1 0%',
+        flex: '0 1 auto',
         marginTop: '20px',
-        marginBottom: '20px'
+        marginBottom: '20px',
+        marginRight: '10px'
+      },
+      '& .o-info__9 span': {
+        display: 'block'
       },
       '& .o-info__8': {
-        flex: '1 1 0%'
+        flex: '1 1 0%',
+        display: 'flex',
+        alignItems: 'center'
       },
       '& .green': {
         color: 'green'
