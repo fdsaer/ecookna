96597d4160feb34647e0b3af8b42698d4484e8df
Vladimir Stepin <vladimir.stepin@osinit.com>
2022-09-26 10:26:17 +0300

    EC-2363 separate single data component

--- dist/CalcOrder/OfferOrder/Offer59.js
+++ dist/CalcOrder/OfferOrder/Offer59.js
@@ -1,4 +1,5 @@
 import PrnProto from '../../PrnProto.js';
+import getProductsData, { fullSquare, fullWeight } from './OfferData.js';
 const {
   React,
   Box,
@@ -332,28 +333,6 @@ class Offer59 extends PrnProto {
       email_address: '',
       address: ''
     };
-    const productListSvg = products && products.map(product => {
-      if (product.characteristic.cnn_elmnts._obj.length || product.characteristic.coordinates._obj.length) {
-        return product;
-      }
-    }).filter(product => product);
-    const productListExtraItems = products && products.map(product => {
-      if (!product.nom.is_service && !product.characteristic.cnn_elmnts._obj.length && !product.characteristic.coordinates._obj.length) {
-        return product;
-      }
-    }).filter(product => product);
-    const productIsService = products && products.map(product => {
-      if (product.nom.is_service) {
-        return product;
-      }
-    }).filter(product => product);
-
-    const fullSquare = products => products && products.map(product => product.s * product.quantity).reduce((acc, productSquare) => acc += productSquare, 0).round(2);
-
-    const getProductWeight = product => product.characteristic.constructions.map(construction => product.characteristic.elm_weight(-1 * construction.cnstr)).reduce((acc, constructionWeight) => acc += constructionWeight, 0);
-
-    const fullWeight = products => products && products.map(product => getProductWeight(product) * product.quantity).reduce((acc, productWeight) => acc += productWeight, 0).round(2);
-
     const order = `Заполнения заказа №${obj.number_doc} от ${moment(obj.date).format('DD MMMM YYYY')} г.`;
     let loading = '';
     const productList = products && products.map(product => {
@@ -370,199 +349,46 @@ class Offer59 extends PrnProto {
         };
       }
     }).filter(product => product);
+    const productTableData = products && getProductsData(products);
 
-    const productsTotalPrice = products => products && products.map(product => product.price * product.quantity).reduce((acc, price) => acc += price, 0).round(0);
+    const getManagerInfo = () => {
+      obj.manager.contact_information.forEach(row => {
+        switch (row.type.name) {
+          case 'Адрес':
+            if (row.presentation && !manager.address) {
+              manager.address = row.presentation;
+            }
 
-    const productsTotalDiscount = products => products && products.map(product => product.price * product.quantity * product.discount).reduce((acc, discount) => acc += discount, 0);
+            break;
 
-    const productsTotalSum = products => products && products.map(product => product.price * product.quantity * (1 - product.discount)).reduce((acc, price) => acc += price, 0).round(0);
+          case 'Телефон':
+            if (row.presentation && !manager.phone_number) {
+              manager.phone_number = row.presentation;
+            }
 
-    const productsTotalQuantity = (products) => products && products.map(product => product.quantity).reduce((acc, quantity) => acc += quantity, 0);
+            break;
 
-    const productTableData = {
-      head: [{
-        text: 'Название',
-        width: '25%',
-        id: 0
-      }, {
-        text: 'Цвет',
-        width: 'auto',
-        id: 1
-      }, {
-        text: 'Количество (шт.)',
-        width: '13%',
-        id: 2
-      }, {
-        text: 'Общий вес (кг)',
-        width: '13%',
-        id: 3
-      }, {
-        text: 'Общая площадь (м2)',
-        width: '13%',
-        id: 4
-      }, {
-        text: 'Цена без скидки (руб.)',
-        width: '13%',
-        id: 5
-      }, {
-        text: 'Скидка (%)',
-        width: '13%',
-        id: 6
-      }, {
-        text: 'Цена со скидкой (руб.)',
-        width: '13%',
-        id: 7
-      }],
-      headExtraItem: [{
-        text: 'Название',
-        width: '25%',
-        id: 0
-      }, {
-        text: 'Количество (шт.)',
-        width: '13%',
-        id: 1
-      }, {
-        text: 'Цена без скидки (руб.)',
-        width: '13%',
-        id: 2
-      }, {
-        text: 'Скидка (%)',
-        width: '13%',
-        id: 3
-      }, {
-        text: 'Цена со скидкой (руб.)',
-        width: '13%',
-        id: 4
-      }],
-      headService: [{
-        text: 'Название',
-        width: '25%',
-        id: 0
-      }, {
-        text: 'Цена без скидки (руб.)',
-        width: '13%',
-        id: 1
-      }, {
-        text: 'Скидка (%)',
-        width: '13%',
-        id: 2
-      }, {
-        text: 'Цена со скидкой (руб.)',
-        width: '13%',
-        id: 3
-      }],
-      rows: productListSvg && productListSvg.map(product => {
-        return [{
-          text: product.characteristic.prod_nom.name_full ? product.characteristic.prod_nom.name_full : product.characteristic.sys.name,
-          id: 0
-        }, {
-          text: product.characteristic.clr.presentation,
-          id: 1
-        }, {
-          text: product.quantity.round(0),
-          id: 2
-        }, {
-          text: (getProductWeight(product) * product.quantity).round(2),
-          id: 4
-        }, {
-          text: (product.s * product.quantity).round(2),
-          id: 3
-        }, {
-          text: (product.price * product.quantity).round(0),
-          id: 5
-        }, {
-          text: (product.price * product.discount).round(0),
-          id: 6
-        }, {
-          text: (product.price * product.quantity * (1 - product.discount)).round(0),
-          id: 7
-        }];
-      }),
-      rowsExtraItem: productListExtraItems && productListExtraItems.map(product => {
-        return [{
-          text: product.characteristic.prod_nom.name_full && product.characteristic.prod_nom.name_full !== 'Аксессуары' ? product.characteristic.prod_nom.name_full : product.characteristic.name,
-          id: 0
-        }, {
-          text: product.quantity.round(0),
-          id: 1
-        }, {
-          text: (product.price * product.quantity).round(0),
-          id: 2
-        }, {
-          text: (product.price * product.discount).round(0),
-          id: 3
-        }, {
-          text: (product.price * product.quantity * (1 - product.discount)).round(0),
-          id: 7
-        }];
-      }),
-      rowsService: productIsService && productIsService.map(product => {
-        return [{
-          text: product.characteristic.prod_nom.name_full ? product.characteristic.prod_nom.name_full : product.nom.name_full,
-          id: 0
-        }, {
-          text: (product.price * product.quantity).round(0),
-          id: 1
-        }, {
-          text: (product.price * product.discount).round(0),
-          id: 2
-        }, {
-          text: (product.price * product.quantity * (1 - product.discount)).round(0),
-          id: 3
-        }];
-      }),
-      total: productListSvg && [{
-        text: 'Всего',
-        id: 0
-      }, {
-        text: productsTotalQuantity(productListSvg),
-        id: 1
-      }, {
-        text: fullWeight(productListSvg),
-        id: 2
-      }, {
-        text: fullSquare(productListSvg),
-        id: 3
-      }, {
-        text: productsTotalPrice(productListSvg),
-        id: 4
-      }, {
-        text: productsTotalDiscount(productListSvg),
-        id: 5
-      }, {
-        text: productsTotalSum(productListSvg),
-        id: 6
-      }],
-      totalExtraItem: productListExtraItems && [{
-        text: 'Всего',
-        id: 0
-      }, {
-        text: productsTotalQuantity(productListExtraItems),
-        id: 1
-      }, {
-        text: productsTotalPrice(productListExtraItems),
-        id: 4
-      }, {
-        text: productsTotalDiscount(productListExtraItems),
-        id: 5
-      }, {
-        text: productsTotalSum(productListExtraItems),
-        id: 6
-      }],
-      totalService: productIsService && [{
-        text: 'Всего',
-        id: 0
-      }, {
-        text: productsTotalPrice(productIsService),
-        id: 3
-      }, {
-        text: productsTotalDiscount(productIsService),
-        id: 4
-      }, {
-        text: productsTotalSum(productIsService),
-        id: 5
-      }]
+          case 'АдресЭлектроннойПочты':
+            if (row.presentation && !manager.email_address) {
+              manager.email_address = row.presentation;
+            }
+
+            break;
+
+          default:
+        }
+      });
     };
+
+    console.log('$$$$$$$$$$$$$$$$$$$');
+    console.log(obj.manager);
+    console.log(obj.manager.class_name);
+    obj.manager.contact_information.forEach(row => console.log(row));
+
+    $p.CatUsers.getAddress = function () {
+      console.log('hello');
+    };
+
     obj.manager.contact_information.forEach(row => {
       switch (row.type.name) {
         case 'Адрес':
@@ -660,7 +486,7 @@ class Offer59 extends PrnProto {
     })), React.createElement(Box, {
       mt: 5,
       className: `${classes?.avoidBreakInside} ${classes?.breakElementWithMargins}`
-    }, productListExtraItems && productListExtraItems.length > 0 && React.createElement(React.Fragment, null, _ref2, components?.ProductsTable && React.createElement(components.ProductsTable, {
+    }, components?.ProductsTable && productTableData.rowsExtraItem.length > 0 && React.createElement(React.Fragment, null, _ref2, React.createElement(components.ProductsTable, {
       head: productTableData.headExtraItem,
       rows: productTableData.rowsExtraItem,
       total: productTableData.totalExtraItem,
@@ -668,7 +494,7 @@ class Offer59 extends PrnProto {
     }))), React.createElement(Box, {
       mt: 5,
       className: `${classes?.avoidBreakInside} ${classes?.breakElementWithMargins}`
-    }, productIsService && productIsService.length > 0 && React.createElement(React.Fragment, null, _ref3, components?.ProductsTable && React.createElement(components.ProductsTable, {
+    }, components?.ProductsTable && productTableData.rowsService.length > 0 && React.createElement(React.Fragment, null, _ref3, React.createElement(components.ProductsTable, {
       head: productTableData.headService,
       rows: productTableData.rowsService,
       total: productTableData.totalService,
