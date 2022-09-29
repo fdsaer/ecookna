eeec38cf77d00f437fe3d530be6adf390cb1b5c9
Vladimir Stepin <vladimir.stepin@osinit.com>
2022-09-29 15:47:03 +0300

    EC-2363 add titles dictionary

--- src/PrnProto.js
+++ src/PrnProto.js
@@ -22,6 +22,34 @@ const removePrintStyles = (selector) => {
     head.removeChild(styleTag);
   });
 };
+
+function add_table_titles(msg) {
+  // публичные методы, экспортируемые, как свойства $p.msg
+  msg.printing_form = {
+    table_columns: {
+      label: 'Название',
+      color: 'Цвет',
+      quantity: 'Количество (шт.)',
+      weight: 'Общий вес (кг)',
+      square: 'Общая площадь (м2)',
+      price: 'Цена без скидки (руб.)',
+      discount: 'Скидка (%)',
+      final_price: 'Цена со скидкой (руб.)',
+      total: 'Всего',
+    },
+    additions_labels: {
+      garage_gate: 'Гаражные ворота',
+      balcony_decoration: 'Отделка балконов',
+      curtains: 'Жалюзи или рольшторы',
+      heating_radiator: 'Декоративные экраны на батареи',
+      evolving_opacity: 'Окна с изменяющейся прозрачностью',
+      orangery: 'Зимний сад или остекленные веранды',
+      glass_door: 'Цельностеклянные межкомнатные двери',
+      glass_heater: 'Обогреватели и полотенцесушители из стекла',
+      phone_charger: 'Зарядку для смартфона встроенного в подоконник',
+    },
+  };
+}
 class PrnProto extends React.Component {
   constructor(props) {
     super(props);
@@ -31,6 +59,9 @@ class PrnProto extends React.Component {
       addPrintStyles('data-custom-print');
       this.classes = classes;
       props.copyStyles && props.copyStyles();
+    };
+    this.componentWillMount = () => {
+      add_table_titles($p.msg);
     };
     this.componentWillUnmount = () => {
       removePrintStyles('[data-custom-print]');
